7b4b314eda616d86b9878a25da8545543346b036
helloguy123ff <sadaraujo@gmail.com>
2024-06-18 18:16:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const speakButton = document.getElementById('speakBtn');
 
     speakButton.addEventListener('click', function() {
-        const message = "Olá! Como posso ajudar você hoje?";
+        const message = "Olá! Como posso ajudar você hoje? Eu estou em minha versão primária, ou seja, não posso fazer muita coisa ainda.";
         speak(message);
     });
 
